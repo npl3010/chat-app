d8623f816c49b41ee4b35bef0f70d29042d620d8
Linh Nguyen <nguyenphuclinh3010@gmail.com>
2021-12-22 16:54:28 +0700

    Example of using Redux Toolkit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from './features/counter/counterSlice';
 import './styles/scss/components/App.scss';
 
 function App() {
+  const count = useSelector(state => state.counter.value);
+  const dispatch = useDispatch();
+
+  const handleDecrease = () => {
+    const action = decrement();
+    dispatch(action);
+  }
+
+  const handleIncrease = () => {
+    const action = increment();
+    dispatch(action);
+  }
+
   return (
     <div className="App">
-      Hello, world!
+      <div>Value: {count}</div>
+      <button onClick={() => handleDecrease()}>Decrease</button>
+      <button onClick={() => handleIncrease()}>Increase</button>
     </div>
   );
 }
---
+++ src/features/counter/counterSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    value: 0,
+}
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment: (state, action) => {
+            state.value += 1;
+        },
+        decrement: (state, action) => {
+            state.value -= 1;
+        },
+    },
+    extraReducers: {}
+});
+
+// Action creators are generated for each case reducer function:
+export const { increment, decrement } = counterSlice.actions
+// Slice Reducer:
+export default counterSlice.reducer;
